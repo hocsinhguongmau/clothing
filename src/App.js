70709a0abeaa3594476194293f2e95eea1ac9acf
thang241190 <thangnguyen24111990@gmail.com>
2020-05-27 17:27:35 +0300

    Finish sign in with google

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import ShopPage from "./pages/shop/shop.component";
 import SignInAndSignUp from "./pages/sign-in-and-sign-up/sign-in-and-sign-up.component";
 import Header from "./components/header/header.component";
 
-import {auth} from "./firebase/firebase.utils.js";
+import {auth, createUserProfileDocument} from "./firebase/firebase.utils.js";
 
 class App extends Component {
 	constructor(props) {
@@ -19,9 +19,20 @@ class App extends Component {
 	}
 	unsubscribeFromAuth = null;
 	componentDidMount() {
-		this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
-			this.setState({currentUser: user});
-			console.log(user);
+		this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
+			if (userAuth) {
+				const userRef = await createUserProfileDocument(userAuth);
+				userRef.onSnapshot(snapShot => {
+					this.setState({
+						currentUser: {
+							id: snapShot.id,
+							...snapShot.data()
+						}
+					});
+				});
+			} else {
+				this.setState({currentUser: userAuth});
+			}
 		});
 	}
 	componentWillUnmount() {
@@ -30,7 +41,7 @@ class App extends Component {
 	render() {
 		return (
 			<div>
-				<Header currentUser={this.state.currentUser}/>
+				<Header currentUser={this.state.currentUser} />
 				<Switch>
 					<Route exact path="/" component={HomePage} />
 					<Route path="/shop" component={ShopPage} />
