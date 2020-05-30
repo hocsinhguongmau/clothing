7eadeb760a4a72c259491b454d3cc02954bf023f
thang241190 <thangnguyen24111990@gmail.com>
2020-05-30 17:11:14 +0300

    add sign up

--- src/App.js
+++ src/App.js
@@ -29,6 +29,8 @@ class App extends Component {
 							...snapShot.data()
 						}
 					});
+					console.log(this.state);
+					
 				});
 			} else {
 				this.setState({currentUser: userAuth});
--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -3,44 +3,47 @@ import "firebase/firestore";
 import "firebase/auth";
 
 const config = {
-	apiKey: "AIzaSyBgASwd9IGltPF-3sogQa7jzRVPQ9Nu4I0",
-	authDomain: "crwn-db-cc2d4.firebaseapp.com",
-	databaseURL: "https://crwn-db-cc2d4.firebaseio.com",
-	projectId: "crwn-db-cc2d4",
-	storageBucket: "crwn-db-cc2d4.appspot.com",
-	messagingSenderId: "671738233252",
-	appId: "1:671738233252:web:db69a96aac0174168d8131",
-	measurementId: "G-MCPQQ6SPPW"
+    apiKey: "AIzaSyBgASwd9IGltPF-3sogQa7jzRVPQ9Nu4I0",
+    authDomain: "crwn-db-cc2d4.firebaseapp.com",
+    databaseURL: "https://crwn-db-cc2d4.firebaseio.com",
+    projectId: "crwn-db-cc2d4",
+    storageBucket: "crwn-db-cc2d4.appspot.com",
+    messagingSenderId: "671738233252",
+    appId: "1:671738233252:web:db69a96aac0174168d8131",
+    measurementId: "G-MCPQQ6SPPW"
 };
 
 firebase.initializeApp(config);
 
-export const createUserProfileDocument = async (userAuth, additionalData) => {
-	if (!userAuth) return;
-	const userRef = firestore.doc(`users/${userAuth.uid}`);
-	const snapShot = await userRef.get();
-	if (!snapShot.exists) {
-		const {displayName, email} = userAuth;
-		const createAt = new Date();
+export const createUserProfileDocument = async(userAuth, additionalData) => {
+    if (!userAuth) 
+        return;
+    const userRef = firestore.doc(`users/${userAuth.uid}`);
+    const snapShot = await userRef.get();
+    if (!snapShot.exists) {
+        const {displayName, email} = userAuth;
+        const createAt = new Date();
 
-		try {
-			await userRef.set({
-				displayName,
-				email,
-				createAt,
-				...additionalData
-			});
-		} catch (error) {
-			console.log("error creating user", error.message);
-		}
-	}
-	return userRef;
+        try {
+            await userRef.set({
+                displayName,
+                email,
+                createAt,
+                ...additionalData
+            });
+        } catch (error) {
+            console.log("error creating user", error.message);
+        }
+    }
+    return userRef;
 };
 
 export const auth = firebase.auth();
 export const firestore = firebase.firestore();
 
-const provider = new firebase.auth.GoogleAuthProvider();
+const provider = new firebase
+    .auth
+    .GoogleAuthProvider();
 provider.setCustomParameters({prompt: "select_account"});
 export const signInWithGoogle = () => auth.signInWithPopup(provider);
 
