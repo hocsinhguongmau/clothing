5c5cea9c32b37daedce29c7a6f2dae5468b38361
thang241190 <thangnguyen24111990@gmail.com>
2020-06-01 17:20:02 +0300

    add redux

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
-import React, {Component} from "react";
-import {Route, Switch} from "react-router-dom";
+import React, { Component } from "react";
+import { Route, Switch } from "react-router-dom";
+import { connect } from "react-redux";
 
 import "./App.css";
 
@@ -8,50 +9,48 @@ import ShopPage from "./pages/shop/shop.component";
 import SignInAndSignUp from "./pages/sign-in-and-sign-up/sign-in-and-sign-up.component";
 import Header from "./components/header/header.component";
 
-import {auth, createUserProfileDocument} from "./firebase/firebase.utils.js";
+import { auth, createUserProfileDocument } from "./firebase/firebase.utils.js";
+
+import { setCurrentUser } from "./redux/user/user.actions";
 
 class App extends Component {
-	constructor(props) {
-		super(props);
-		this.state = {
-			currentUser: null
-		};
-	}
-	unsubscribeFromAuth = null;
-	componentDidMount() {
-		this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
-			if (userAuth) {
-				const userRef = await createUserProfileDocument(userAuth);
-				userRef.onSnapshot(snapShot => {
-					this.setState({
-						currentUser: {
-							id: snapShot.id,
-							...snapShot.data()
-						}
-					});
-					console.log(this.state);
-					
-				});
-			} else {
-				this.setState({currentUser: userAuth});
-			}
-		});
-	}
-	componentWillUnmount() {
-		this.unsubscribeFromAuth();
-	}
-	render() {
-		return (
-			<div>
-				<Header currentUser={this.state.currentUser} />
-				<Switch>
-					<Route exact path="/" component={HomePage} />
-					<Route path="/shop" component={ShopPage} />
-					<Route path="/signin" component={SignInAndSignUp} />
-				</Switch>
-			</div>
-		);
-	}
+  unsubscribeFromAuth = null;
+
+  componentDidMount() {
+    const { setCurrentUser } = this.props;
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async (userAuth) => {
+      if (userAuth) {
+        const userRef = await createUserProfileDocument(userAuth);
+        userRef.onSnapshot((snapShot) => {
+          setCurrentUser({
+            id: snapShot.id,
+            ...snapShot.data()
+          });
+        });
+      } else {
+        setCurrentUser(userAuth);
+      }
+    });
+  }
+  componentWillUnmount() {
+    this.unsubscribeFromAuth();
+  }
+  render() {
+    return (
+      <div>
+        <Header />
+        <Switch>
+          <Route exact path="/" component={HomePage} />
+          <Route path="/shop" component={ShopPage} />
+          <Route path="/signin" component={SignInAndSignUp} />
+        </Switch>
+      </div>
+    );
+  }
 }
 
-export default App;
+const mapDispatchToProps = (dispatch) => ({
+  setCurrentUser: (user) => dispatch(setCurrentUser(user))
+});
+
+export default connect(null, mapDispatchToProps)(App);
--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,33 +1,39 @@
 import React from "react";
-import {Link} from "react-router-dom";
-import {auth} from "../../firebase/firebase.utils.js";
-
+import { Link } from "react-router-dom";
+import { connect } from "react-redux";
+import { auth } from "../../firebase/firebase.utils.js";
 import "./header.styles.scss";
-import {ReactComponent as Logo} from "../../assets/images/logo.svg";
+import { ReactComponent as Logo } from "../../assets/images/logo.svg";
 
-const Header = ({currentUser}) => (
-	<div className="header">
-		<Link className="logo-container" to="/">
-			<Logo className="logo" />
-		</Link>
-		<div className="options">
-			<Link className="option" to="/shop">
-				SHOP
-			</Link>
-			<Link className="option" to="/contact">
-				CONTACT
-			</Link>
-			{currentUser ? (
-				<div>
-					<div className="option" onClick={() => auth.signOut()}>
-						SIGN OUT
-					</div>
-				</div>
-			) : (
-				<Link className="option" to="/signin">SIGN IN</Link>
-			)}
-		</div>
-	</div>
+const Header = ({ currentUser }) => (
+  <div className="header">
+    <Link className="logo-container" to="/">
+      <Logo className="logo" />
+    </Link>
+    <div className="options">
+      <Link className="option" to="/shop">
+        SHOP
+      </Link>
+      <Link className="option" to="/contact">
+        CONTACT
+      </Link>
+      {currentUser ? (
+        <div>
+          <div className="option" onClick={() => auth.signOut()}>
+            SIGN OUT
+          </div>
+        </div>
+      ) : (
+        <Link className="option" to="/signin">
+          SIGN IN
+        </Link>
+      )}
+    </div>
+  </div>
 );
 
-export default Header;
+const mapStateToProps = (state) => ({
+  currentUser: state.user.currentUser
+});
+
+export default connect(mapStateToProps)(Header);
