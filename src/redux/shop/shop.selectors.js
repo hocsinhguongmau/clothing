b89687db8d2a28d8b3c79885e3422e8cf972c5d4
thang241190 <thangnguyen24111990@gmail.com>
2020-06-30 00:33:31 +0300

    create selectors and update components with new ux flow for directory/shop

--- src/redux/shop/shop.selectors.js
+++ src/redux/shop/shop.selectors.js
@@ -6,3 +6,14 @@ export const selectCollections = createSelector(
   [selectShop],
   (shop) => shop.collections
 );
+
+export const selectCollectionsForPreview = createSelector(
+  [selectCollections],
+  (collections) => Object.keys(collections).map((keys) => collections[keys])
+);
+
+export const selectCollection = (collectionUrlParam) =>
+  createSelector(
+    [selectCollections],
+    (collections) => collections[collectionUrlParam]
+  );
